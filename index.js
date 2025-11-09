d1c65cd536fdd97e2b7b7f6d52e876aa67306b79
Ashik <ashikurashik20999@gmail.com>
2025-11-09 16:41:09 +0600

    Setup dotenv and MongoDB authentication issue

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// SgtJkznyJW1ADxfk foodLoverDBUser
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fj5vaxe.mongodb.net/?appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Checking Local Food Lovers Network Server');
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        // const db = client.db('localFoodDB');
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Local Food Lover Network Server app is listening on port ${port}`);
+})
